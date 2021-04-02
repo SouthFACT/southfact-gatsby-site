82ada709550043b92fa5fe4e5757d9e1c5b8f380
Jeff Bliss <jeff.a.bliss@gmail.com>
2021-04-02 11:40:55 -0400

    Link color is much more readable

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,24 @@
 import React from "react"
 import Layout from "../components/layout"
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  anchorTag: {
+    color: 'gold',
+  },
+}));
 
 export default function Contact() {
-  return (
-    <Layout>
-      <Typography variant="h3">
-        Contact
-      </Typography>
-      <Typography variant="h5">
-        If you have any questions, comments, or concerns please send an email to info@southfact.com 
-      </Typography>
-    </Layout>
-  )
+  const classes = useStyles();
+    return (
+      <Layout>
+        <Typography variant="h3">
+          Contact
+        </Typography>
+        <Typography variant="h5">
+          If you have any questions, comments, or concerns please send an email to <a className={classes.anchorTag} href='mailto:info@southfact.com'>info@southfact.com</a> 
+        </Typography>
+      </Layout>
+    )
 }
