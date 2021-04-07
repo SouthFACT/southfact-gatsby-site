8a41d9c6357901e3217ef09b4c2af505f072cdfa
Jeff Bliss <jeff.a.bliss@gmail.com>
2021-04-07 14:31:30 -0400

    added global stylesheet. We should only use this when absolutely necessary

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,23 +1,15 @@
 import React from "react"
 import Layout from "../components/layout"
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  anchorTag: {
-    color: 'gold',
-  },
-}));
 
 export default function Contact() {
-  const classes = useStyles();
     return (
       <Layout>
         <Typography variant="h3">
           Contact
         </Typography>
         <Typography variant="h5">
-          If you have any questions, comments, or concerns please send an email to <a className={classes.anchorTag} href='mailto:info@southfact.com'>info@southfact.com</a> 
+          If you have any questions, comments, or concerns please send an email to <a href='mailto:info@southfact.com'>info@southfact.com</a> 
         </Typography>
       </Layout>
     )
