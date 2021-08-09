74b4eb232400770de7c19d486759a08de2b9f139
Dave Michelson <daveism@gmail.com>
2021-08-09 14:05:45 -0400

    changes to test action

--- src/components/downloadLinks.js
+++ src/components/downloadLinks.js
@@ -8,6 +8,14 @@ const DownloadLinks = [
     "year":"2020"
   },
   {
+    "geographylevel":"Georgia",
+    "datatype":"State Wide Change Polygons",
+    "size":"45MB",
+    "link":"https://nemac.unca.edu/home/what-we-do/",
+    "method":"swir",
+    "year":"2020"
+  },
+  {
     "geographylevel":"North Carolina",
     "datatype":"State Wide Change Polygons",
     "size":"40MB",
