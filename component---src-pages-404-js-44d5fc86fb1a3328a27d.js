383b288def63d7b7ef975bc43cf3fe84f85c350d
Jeff Bliss <jeff.a.bliss@gmail.com>
2022-07-21 13:50:34 -0400

    Updates

--- component---src-pages-404-js-44d5fc86fb1a3328a27d.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksouthfact=self.webpackChunksouthfact||[]).push([[883],{429:function(e,t,n){n.r(t);var u=n(7294),l=n(1597),a=n(3645);t.default=function(){return u.createElement(a.Z,null,u.createElement("h1",null,"Page not found"),u.createElement("p",null,u.createElement(l.rU,{to:"/"},"Head home")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-44d5fc86fb1a3328a27d.js.map
